8f86addcf7dd163b7106b77069f68dde4877c7c0
Jamie Nuttall <jamie@wattu.com>
2020-12-19 09:23:26 -0800

    restart

---
+++ restart.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# return GPIO input status
+
+# select pin
+GPIO=13
+
+# prepare the pin
+if [ ! -d /sys/class/gpio/gpio${GPIO} ]; then
+  echo "${GPIO}" > /sys/class/gpio/export
+fi
+echo "in" > /sys/class/gpio/gpio"${GPIO}"/direction
+
+# continuously monitor current value
+while true; do
+  if [ 1 == "$(</sys/class/gpio/gpio"${GPIO}"/value)" ]; then
+    printf "Low \r"
+    sleep 1
+  else
+    printf "Restarting stream \r"
+    systemctl restart stream
+    sleep 5
+  fi
+done
